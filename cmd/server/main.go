a02cba8bd3917aaa8567e39d68506f11619062f5
MishaMgla <mikhail.avangarde@gmail.com>
2026-06-11 22:46:33 +0300

    feat: scaffold Go server serving static web client

---
+++ cmd/server/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir("web")))
+	log.Println("listening on :8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}
