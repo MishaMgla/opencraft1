39b3c5fd0b91582d5a42f0d0d8b576cba2c1e69c
MishaMgla <mikhail.avangarde@gmail.com>
2026-06-11 22:59:50 +0300

    feat: wire up sim + websocket server in main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+
+	"opencraft/internal/server"
+	"opencraft/internal/world"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir("web")))
-	log.Println("listening on :8080")
-	if err := http.ListenAndServe(":8080", mux); err != nil {
-		log.Fatal(err)
-	}
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	sim := world.NewSim()
+	go sim.Run(ctx)
+
+	httpSrv := &http.Server{Addr: ":8080", Handler: server.New(sim).Handler()}
+	go func() {
+		log.Println("listening on :8080")
+		if err := httpSrv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	<-ctx.Done()
+	log.Println("shutting down")
+	httpSrv.Close()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module opencraft
 
 go 1.23
 
-require github.com/coder/websocket v1.8.14 // indirect
+require github.com/coder/websocket v1.8.14
